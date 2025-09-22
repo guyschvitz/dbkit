a0cb4874692f5f3a84c0a917f1c46a3167be3a59
Guy Schvitz <guy.schvitz@ec.europa.eu>
2025-09-22 16:48:48 +0200

    fixed connect2Db.R issue

--- R/connect2Db.R
+++ R/connect2Db.R
@@ -40,3 +40,27 @@
 #'   drv = RPostgreSQL::PostgreSQL()
 #' )
 #' }
+connect2Db <- function(db.info.ls, drv = odbc::odbc()) {
+
+  # Validate required parameters
+  required.fields <- c("driver", "server", "database", "username", "password")
+  missing.fields <- setdiff(required.fields, names(db.info.ls))
+
+  if (length(missing.fields) > 0) {
+    stop("Missing required fields in db.info.ls: ", paste(missing.fields, collapse = ", "))
+  }
+
+  # Create database connection
+  con <- DBI::dbConnect(
+    drv = drv,
+    Driver = db.info.ls$driver,
+    Server = db.info.ls$server,
+    Database = db.info.ls$database,
+    UID = db.info.ls$username,
+    PWD = db.info.ls$password,
+    TrustServerCertificate = "yes"
+  )
+
+  return(con)
+}
+
